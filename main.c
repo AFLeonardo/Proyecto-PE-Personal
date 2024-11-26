792c08ddb551e246f05749bad4dbbd96103a9c3c
Leonardo <alfonsorruiz012@gmail.com>
2024-11-26 12:57:03 -0600

    Mejorando legibilidad del codigo.

--- main.c
+++ main.c
@@ -5,39 +5,51 @@
 #include "funciones.c"
 #include <locale.h>
 
-main()
+main() 
 {
     setlocale(LC_ALL, "");
     int opcion;
     bool ciclo = true;
     FILE *archivo;
-    struct Articulos articuloVacio = {0, "", "", "", {0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0}, 0, 0, 0};
-    struct Insumo insumoVacio = {0, "", 0, 0, {0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0}};
-    struct Proveedor proveedorVacio = {0, "", "", "", 0, 0, 0, 0, "", {0,0,0,0,0,0,0,0,0,0}};
+
+    struct Articulos articuloVacio = {0, "", "", "", 
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 0, 0, 0
+    };
+
+    struct Insumo insumoVacio = {0, "", 0, 0, 
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    };
+
+    struct Proveedor proveedorVacio = {0, "", "", "", 0, 0, 0, 0, "", 
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    };
+
     struct Mercado mercadoVacio = {0, "", "", "", 0, 0, 0, 0, ""};
-    struct Empleado empleadoVacio = {0, "", "", "", 0.0, {0, 0, 0}, {"", 0, "", "", ""}};
 
+    struct Empleado empleadoVacio = {0, "", "", "", 0.0, 
+        {0, 0, 0}, {"", 0, "", "", ""}
+    };
 
-    while (ciclo)
+    while (ciclo) 
     {
-
-        do
+        do 
         {
             opcion = menu_principal();
             if (opcion < 0 || opcion > 10)
-                printf("Opcion no valida. Intenta de nuevo\n");
-
+                printf("Opción no válida. Intenta de nuevo\n");
         } while (opcion < 0 || opcion > 10);
 
-        switch (opcion)
+        switch (opcion) 
         {
             case 1:
-                if((archivo = fopen("Articulos.dat", "r+")) == NULL)
+                if ((archivo = fopen("Articulos.dat", "r+")) == NULL) 
                 {
-                    printf("ERROR.\nSe esta creando el archivo intenta de nuevo.\n");
+                    printf("ERROR.\nSe está creando el archivo. Intenta de nuevo.\n");
                     crearRegistrosVacios("Articulos.dat", &articuloVacio, sizeof(struct Articulos), 1000);
-                }
-                else
+                } 
+                else 
                 {
                     menu_articulos(archivo);
                     fclose(archivo);
@@ -46,12 +58,12 @@ main()
                 break;
 
             case 2:
-                if ((archivo = fopen("Insumos.dat", "r+")) == NULL)
+                if ((archivo = fopen("Insumos.dat", "r+")) == NULL) 
                 {
-                    printf("ERROR.\nSe esta creando el archivo intenta de nuevo.\n");
+                    printf("ERROR.\nSe está creando el archivo. Intenta de nuevo.\n");
                     crearRegistrosVacios("Insumos.dat", &insumoVacio, sizeof(struct Insumo), 100);
-                }
-                else
+                } 
+                else 
                 {
                     menu_insumos(archivo);
                     fclose(archivo);
@@ -60,12 +72,12 @@ main()
                 break;
 
             case 3:
-                if ((archivo = fopen("Mercados.dat", "r+")) == NULL)
+                if ((archivo = fopen("Mercados.dat", "r+")) == NULL) 
                 {
-                    printf("ERROR.\nSe esta creando el archivo intenta de nuevo.\n");
+                    printf("ERROR.\nSe está creando el archivo. Intenta de nuevo.\n");
                     crearRegistrosVacios("Mercados.dat", &mercadoVacio, sizeof(struct Mercado), 100);
-                }
-                else
+                } 
+                else 
                 {
                     menu_mercados(archivo);
                     fclose(archivo);
@@ -73,25 +85,25 @@ main()
                 break;
 
             case 4:
-                if ((archivo = fopen("Empleados.dat", "r+")) == NULL)
+                if ((archivo = fopen("Empleados.dat", "r+")) == NULL) 
                 {
-                    printf("ERROR.\nSe esta creando el archivo intenta de nuevo.\n");
-					crearRegistrosVacios("Empleados.dat", &empleadoVacio, sizeof(struct Empleado), 1000);
-				}
-                else
+                    printf("ERROR.\nSe está creando el archivo. Intenta de nuevo.\n");
+                    crearRegistrosVacios("Empleados.dat", &empleadoVacio, sizeof(struct Empleado), 1000);
+                } 
+                else 
                 {
                     menu_empleados(archivo);
                     fclose(archivo);
                 }
                 break;
 
-            case 5: 
-                if((archivo = fopen("Proveedores.dat", "r+")) == NULL)
+            case 5:
+                if ((archivo = fopen("Proveedores.dat", "r+")) == NULL) 
                 {
-                    printf("ERROR.\nSe esta creando el archivo intenta de nuevo.\n");
+                    printf("ERROR.\nSe está creando el archivo. Intenta de nuevo.\n");
                     crearRegistrosVacios("Proveedores.dat", &proveedorVacio, sizeof(struct Proveedor), 100);
-                }
-                else
+                } 
+                else 
                 {
                     menu_proveedores(archivo);
                     fclose(archivo);
@@ -100,9 +112,9 @@ main()
                 break;
 
             case 6:
-                if((archivo = fopen("Ventas.txt", "a")) == NULL)
-                    printf("ERROR.\nSe esta creando el archivo intenta de nuevo.\n");
-                else
+                if ((archivo = fopen("Ventas.txt", "a")) == NULL) 
+                    printf("ERROR.\nSe está creando el archivo. Intenta de nuevo.\n");
+                else 
                 {
                     menu_control_ventas(archivo);
                     fclose(archivo);
@@ -111,9 +123,9 @@ main()
                 break;
 
             case 7:
-                if ((archivo = fopen("Compras.txt", "a+")) == NULL)
-                    printf("ERROR \nSe esta creando el archivo intenta de nuevo\n");
-                else
+                if ((archivo = fopen("Compras.txt", "a+")) == NULL) 
+                    printf("ERROR.\nSe está creando el archivo. Intenta de nuevo.\n");
+                else 
                 {
                     menu_control_compras(archivo);
                     fclose(archivo);
@@ -121,9 +133,9 @@ main()
                 break;
 
             case 8:
-                if((archivo = fopen("Compras.txt", "r")) == NULL)
+                if ((archivo = fopen("Compras.txt", "r")) == NULL) 
                     printf("ERROR.\nNo se pudo abrir el archivo.\n");
-                else
+                else 
                 {
                     menu_control_inventario(archivo);
                     fclose(archivo);
@@ -135,11 +147,9 @@ main()
                 break;
 
             case 10:
-                printf("%s\n", "HA SALIDO DEL SISTEMA.\n");
+                printf("HA SALIDO DEL SISTEMA.\n");
                 ciclo = false;
                 break;
         }
-
     }
-    return 0;
 }
